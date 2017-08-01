9c273573db45f2c1cf0a58b0e476c8efd63a894d
AkshatSrivastava2 <akshatsrivastava2@gmail.com>
2017-08-01 22:05:42 +0530

    Recipe list adding done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React,{ Component } from 'react';
 import SearchRecipes from './SearchRecipes';
 import '../styles/App.css';
+import RecipeList from './RecipeList';
+
 class App extends Component{
     render(){
         return(
             <div>
                 <h2>Recipe Finder</h2>
                 <SearchRecipes />
+                <RecipeList />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducer';
 
 const store=createStore(rootReducer);
-store.subscribe(()=>console.log(store.getState()));
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
